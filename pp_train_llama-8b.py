b046749133243f6d5501e55301d84a7ec19c6610
jaehong park <iworkist@gmail.com>
2025-04-17 15:50:08 +0900

    Update pp_train_llama-8b.py

--- pp_train_llama-8b.py
+++ pp_train_llama-8b.py
@@ -116,7 +116,7 @@ print(f"nbatches={nbatches}")
 # TensorBoard SummaryWriter
 # runs/date-time folder
 if optimus_p.is_last_stage():
-    log_dir = os.path.join("runs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    log_dir = os.path.join("/workspace/runs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     writer = SummaryWriter(log_dir=log_dir)
 
 epochs = 1 # The number of epochs
